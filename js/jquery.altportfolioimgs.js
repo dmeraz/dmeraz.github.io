d5416eba1ae8c1b4bafdefb6d8f6451b15544c72
dmeraz <darin.meraz@gmail.com>
2016-10-26 11:09:28 -0400

    initial commit

---
+++ js/jquery.altportfolioimgs.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () { //Script commented out, see comment in indez.html
+    $("#code1").mouseover(400).fadeOut(function () {
+        $(this).attr('src', 'images/virtual-pet-code.png').fadeIn(500);
+    });
+});
